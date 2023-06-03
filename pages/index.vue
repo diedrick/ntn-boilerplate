de09e6e21c5e0d2e13c380559dbd80bb1a853c2a
Michael Diedrick <michael@bytestudios.com>
2023-06-03 13:50:13 -0500

    Simple changes.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
-      <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
-      <h2 class="subtitle text-center">Boilerplate</h2>
+      <h1 class="title text-center">Welcome, mathlete.</h1>
+      <h2 class="subtitle text-center">Trying to figure out how data works in this nuxt, tailwind, netlify CMS site.</h2>
     </section>
 
     <section class="mt-8">
